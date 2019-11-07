350946042885b68baac10dd14d0c73e0c4767359
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-07 08:57:20 -0700

    Add mapDispatchToProps to App

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { getAnimals, getDonations } from '../../apiCalls';
+import { listedAnimals, receivedDonations } from '../../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 class App extends Component {
   async componentDidMount() {
       try {
           const resp = await getAnimals;
-
+          this.props.listedAnimals(resp);
           this.appendDonations()
       } catch(error) {
           console.log('Error getting animals', error);
@@ -15,7 +18,7 @@ class App extends Component {
   appendDonations = async () => {
     try {
         const resp = await getDonations;
-      
+        this.props.receivedDonations(resp);
     } catch(error) {
         console.log('Error getting donations', error);
     }
@@ -30,4 +33,11 @@ class App extends Component {
   }
 }
 
-export default App;
+export const mapDispatchToProps = dispatch => (
+    bindActionCreators({ 
+        listedAnimals, 
+        receivedDonations
+    }, dispatch)
+  );
+
+export default connect(null, mapDispatchToProps)(App);
