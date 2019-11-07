a1e6cf995dfcc8ff6f6faf28f5fa888d60ad772f
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-07 08:28:50 -0700

    Add fetches to apiCalls

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,1 +1,16 @@
+import React, { Component } from 'react';
 
+
+class App extends Component {
+  
+
+  render() {
+    return (
+        <main>
+
+        </main>
+    )
+  }
+}
+
+export default App;
