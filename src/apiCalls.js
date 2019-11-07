bc5e2b4c6ee8d99f4afdc1ba220116665a2bd457
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-07 08:31:30 -0700

    Add fetches

---
+++ src/apiCalls.js
@@ -0,0 +1,40 @@
+export const getAnimals = async () => {
+    const resp = await fetch('http://localhost:3001/api/v1/rescue-animals');
+    const data = await resp.json();
+  
+    if(!resp.ok) {
+        throw new Error('No animals fetched');
+    } else {
+      return data;
+    }
+  }
+  
+  export const getDonations = async () => {
+      const resp = await fetch('http://localhost:3001/api/v1/donations');
+      const data = await resp.json();
+    
+      if(!resp.ok) {
+          throw new Error('No donations fetched');
+      } else {
+        return data;
+      }
+  }
+  
+  export const postDonations = async (donation) => {
+     const option = {
+       method: 'POST',
+       body: JSON.stringify(donation),
+       headers: {
+           'Content-Type' : 'application/json'
+       }
+     }
+  
+      const resp = await fetch('http://localhost:3001/api/v1/donations', option);
+      const data = await resp.json();
+    
+      if(!resp.ok) {
+          throw new Error('No donations fetched');
+      } else {
+        return data;
+      }
+  }
