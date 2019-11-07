90588e6a65a2b71dc7d212916083928b08a674e0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-07 10:51:33 -0700

    Add mapDispatch test

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import App from './App';
+import App, { mapDispatchToProps } from './App';
 import { shallow } from 'enzyme';
+import { listedAnimals, receivedDonations } from '../../actions';
+import { getAnimals, getDonations } from '../../apiCalls';
+
+jest.mock('../../apiCalls');
 
 describe('App', () => {
-   let wrapper;
-   let mocklistedAnimals = [
-     {id: 1, name:'bob', species:'who knows', description:'who cares'}
-   ];
-   let mockDonations = [
-     {name: 'bob', donation: 0}
-   ]
 
-   beforeEach(() => {
-     wrapper = shallow(<App  
-        listedAnimals={mocklistedAnimals}
-        receivedDonations={mocklistedAnimals}
-     />)
-   })
+  describe('App tests', () => {
+    let wrapper;
+    let mocklistedAnimals = [
+      {id: 1, name:'bob', species:'who knows', description:'who cares'}
+    ];
+    let mockDonations = [
+      {name: 'bob', donation: 0}
+    ];
+ 
+    beforeEach(() => {
+      wrapper = shallow(<App  
+         listedAnimals={mocklistedAnimals}
+         receivedDonations={mockDonations}
+      />)
+    })
+ 
+    it('should match snapshot with all tests passing through correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    })
+  })
+  
+   describe('mapDispatchToProps',  () => {
+      let mockDispatch = jest.fn();
+      let mocklistedAnimals = [
+        {id: 1, name:'bob', species:'who knows', description:'who cares'}
+      ];
+      let mockDonations = [
+        {name: 'bob', donation: 0}
+      ];
 
-   it('should match snapshot with all tests passing through correctly', () => {
-     expect(wrapper).toMatchSnapshot();
-   })
+      getAnimals.mockImplementation(() => {
+        return Promise.resolve(mocklistedAnimals);
+      })
 
+      getDonations.mockImplementation(() => {
+        return Promise.resolve(mockDonations);
+      })
+
+      it('should dispatch listedAnimals when component', () => {
+        const actionToDispatch = listedAnimals(mocklistedAnimals);
+        const mappedProps = mapDispatchToProps(mockDispatch);
+
+        mappedProps.listedAnimals(mocklistedAnimals);
+        expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+      })
+
+      it('should dispatch receivedDonations when component', () => {
+        const actionToDispatch = receivedDonations(mockDonations);
+        const mappedProps = mapDispatchToProps(mockDispatch);
+
+        mappedProps.receivedDonations(mockDonations);
+        expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+      })
+       
+     })
 })
